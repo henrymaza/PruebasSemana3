1b4b166eb18d835b307644d383c1b848caf59200
daniel.pacheco <daniel.pacheco@metalteco.com>
2025-05-04 18:24:44 -0500

    Feat: captura de pantalla de test

--- src/test/java/org/example/RegistroTest.java
+++ src/test/java/org/example/RegistroTest.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import java.io.File;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RegistroTest {
@@ -19,12 +27,37 @@ public class RegistroTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // Crear el directorio de capturas de pantalla si no existe
+        File screenshotsDir = new File("screenshots");
+        if (!screenshotsDir.exists()) {
+            screenshotsDir.mkdirs();
+        }
     }
 
     @BeforeEach
     public void abrirFormulario() {
         File htmlFile = new File("src/test/resources/formulario_registro.html");
         driver.get("file://" + htmlFile.getAbsolutePath());
+    }
+
+    @AfterEach
+    public void capturarPantalla(TestInfo testInfo) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String testName = testInfo.getTestMethod().orElse(null).getName();
+        if (testName != null) {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH_mm_ss");
+            String timestamp = now.format(formatter);
+            String screenshotName = "screenshots" + File.separator + testName.split("_")[0] + "_" + timestamp + ".png";
+            File destination = new File(screenshotName);
+            try {
+                FileUtils.copyFile(source, destination);
+                System.out.println("Captura de pantalla guardada en: " + screenshotName);
+            } catch (IOException e) {
+                System.out.println("Error al guardar la captura de pantalla: " + e.getMessage());
+            }
+        }
     }
 
     @AfterAll
